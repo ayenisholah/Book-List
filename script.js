fac6499d62631281f77165c29632b3d424d8c78a
Shola Ayeni <ayenisholah@yahoo.com>
2019-08-25 10:44:59 +0100

    UI constructor

--- script.js
+++ script.js
@@ -3,4 +3,25 @@ function Book(title, author, isbn) {
   this.title = title;
   this.author = author;
   this.isbn = isbn;
+};
+
+// UI Constructor
+function UI() {  }
+
+UI.prototype.addBookToList = function(book) {
+  console.log(book)
 }
+
+
+// Event Listeners
+document.getElementById('book-form').addEventListener('submit', function(e){
+  // Get Form Values
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const isbn = document.getElementById('isbn').value;
+  e.preventDefault();
+
+  // Instatiate Book
+  const book = new Book(title, author, isbn)
+  console.log(book)
+});
