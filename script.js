c07abee25d3678fe57378571788becc9c8933356
Shola Ayeni <ayenisholah@yahoo.com>
2019-08-25 11:07:34 +0100

    adding book to list

--- script.js
+++ script.js
@@ -6,22 +6,52 @@ function Book(title, author, isbn) {
 };
 
 // UI Constructor
-function UI() {  }
+function UI() { }
 
-UI.prototype.addBookToList = function(book) {
-  console.log(book)
+// Add book to list
+UI.prototype.addBookToList = function (book) {
+  const list = document.getElementById('book-list');
+  // Create Tr element
+  const row = document.createElement('tr');
+  // Insert Cols
+  row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href='#' class='delete'>X</a></td>
+
+  `
+  list.appendChild(row);
+}
+
+// Clear book fields
+UI.prototype.clearFields = function (books) {
+  document.getElementById('title').value = '';
+  document.getElementById('author').value = '';
+  document.getElementById('isbn').value = '';
+
 }
 
 
 // Event Listeners
-document.getElementById('book-form').addEventListener('submit', function(e){
+document.getElementById('book-form').addEventListener('submit', function (e) {
   // Get Form Values
   const title = document.getElementById('title').value;
   const author = document.getElementById('author').value;
   const isbn = document.getElementById('isbn').value;
-  e.preventDefault();
 
   // Instatiate Book
   const book = new Book(title, author, isbn)
-  console.log(book)
+
+  // Instatiate UI
+  const ui = new UI();
+  console.log(ui);
+
+  // Add Book to list
+  ui.addBookToList(book);
+
+  // Clear fields after adding book
+  ui.clearFields(book)
+  e.preventDefault();
+
 });
